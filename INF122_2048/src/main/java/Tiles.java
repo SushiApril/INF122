e93f231162055b1dacaa62965f72809b3e5c823e
“Sushant <sushang1@uci.edu>
2025-03-08 15:56:02 -0800

    Uploading tiles again since previous commits went away somehow

--- INF122_2048/src/main/java/Tiles.java
+++ INF122_2048/src/main/java/Tiles.java
@@ -1,24 +1,54 @@
-// Tiles class which will be stored in Grid
+import java.util.Random;
 
 public class Tiles {
+    // Value of the tile
     public int value;
 
+    // Default constructor: sets value to 0 (empty tile)
     public Tiles() {
-        //default value is 0 
         this.value = 0;
     }
 
-    // get function
+    // Constructor that accepts a value for the tile
+    public Tiles(int value) {
+        this.value = value;
+    }
+
+    // Getter function to retrieve the value of the tile
     public int getValue() {
         return value;
     }
-    //set function
+
+    // Setter function to set the value of the tile
     public void setValue(int value) {
         this.value = value;
     }
 
-    // func to see if its empty, tile is empty if value = 0
+    // Function to check if the tile is empty (value = 0)
     public boolean isEmpty() {
         return value == 0;
+    }
+
+    // Checks if the tile can merge with another tile (same value and not empty)
+    public boolean canMerge(Tiles otherTile) {
+        return otherTile != null && this.value == otherTile.getValue() && this.value != 0;
+    }
+
+    // Generates a new tile with either a 2 or 4 value (random)
+    public static Tiles generateRandomTile() {
+        Random random = new Random();
+        // Generate a random number, either 0 or 1
+        int randomValue = random.nextInt(2);
+
+        // Assign value 2 or 4 based on random value
+        int value = (randomValue == 0) ? 2 : 4;
+        
+        // Return a new tile with the generated value
+        return new Tiles(value);
+    }
+
+    // Resets the tile value to 0 (makes it empty)
+    public void setTilesEmpty() {
+        this.value = 0;
     }
 }
